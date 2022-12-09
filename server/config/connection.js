c95601c17b4ac1dc35cfc17081ccaa2d9156375f
CoderCoding00 <107374333+CoderCoding00@users.noreply.github.com>
2022-12-08 18:31:34 -0800

    updated heroku push

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
 });
 
 module.exports = mongoose.connection;
