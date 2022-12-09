6af36b577acf0805c692bb7ffd3cd311e9cd9aab
CoderCoding00 <107374333+CoderCoding00@users.noreply.github.com>
2022-12-08 21:50:27 -0800

    update connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks');
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/googlebooks',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  );
 
 module.exports = mongoose.connection;
