bf4ea1ba773266802cc434b01ee467b037dde000
CoderCoding00 <107374333+CoderCoding00@users.noreply.github.com>
2022-12-08 23:45:41 -0800

    heroku

--- server/server.js
+++ server/server.js
@@ -35,11 +35,6 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-// ***** TRY THIS get 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
 // *** COMMENT OUT THE ROUTES THAT WERE GIVEN??? 
 // app.use(routes);
 
@@ -48,8 +43,13 @@ const startingApolloServer = async (typeDefs, resolvers) =>{
   await server.start();
   // APPLY THE APOLLO SERVER MIDDLEWARE TO THE EXPRESS APP
   server.applyMiddleware({app});
+  db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+    // LOG WHERE THE GRAPHQL API IS RUNNING WITH THE APOLLO SERVER (graphqlPath)
+    console.log(`Use http://localhost:${PORT}${server.graphqlPath}`);
+  });
+}
   
-
   // **** REPLACED THE ABOVE APOLLO SERVER CODE WITH MONGO SERVER 
   // const startingMongoServer = async (typeDefs, resolvers) =>{
   //   await server.start();
@@ -63,12 +63,7 @@ const startingApolloServer = async (typeDefs, resolvers) =>{
   //     useUnifiedTopology: true,
   //   },
   // );
-  db.once('open', () => {
-    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-    // LOG WHERE THE GRAPHQL API IS RUNNING WITH THE APOLLO SERVER (graphqlPath)
-    console.log(`Use http://localhost:${PORT}${server.graphqlPath}`);
-  });
-}
+
 startingApolloServer(typeDefs, resolvers);
 // REPLACE THE ABOVE APOLLO SERVER CODE WITH MONGO SERVER
 // startingMongoServer(typeDefs, resolvers);
