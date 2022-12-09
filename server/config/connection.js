bde1e1d0a54f5b26b0c7ef1ad73aa9fa6fa9d7fc
CoderCoding00 <107374333+CoderCoding00@users.noreply.github.com>
2022-12-08 23:28:36 -0800

    heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,20 @@
 const mongoose = require('mongoose');
 
+// mongoose.connect(
+//     process.env.MONGODB_URI || 'mongodb://localhost/googlebooks',
+//     {   useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//         useCreateIndex: true,
+//         useFindAndModify: false,
+//     },
+//   );
+
+// module.exports = mongoose.connection;
+
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/googlebooks',
-    {   
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
+    process.env.MONGODB_URI || `mongodb://127.0.0.1:27017/book-search-app`,
+    {   useNewUrlParser: true,
+        useUnifiedTopology: true
     },
   );
 
