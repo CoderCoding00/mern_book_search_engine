35b1117d94e3448a96a51df86c99d14cd073de8e
CoderCoding00 <107374333+CoderCoding00@users.noreply.github.com>
2022-12-08 23:12:42 -0800

    heroku

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -8,11 +8,12 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
+  // *****  TRY ADDIGN signToken AT THE END
+  // signToken: function ({ username, email, _id }) {
+  //   const payload = { username, email, _id };
 
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
+  //   return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  // },
   // function for our authenticated routes
   authMiddleware: function (req) {
     // allows token to be sent via  req.query or headers
@@ -45,5 +46,10 @@ module.exports = {
     // send to next endpoint
     // next();
     return req;
+  },
+   signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
