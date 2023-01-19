18bae6b0520c790943a6a3807c8a990bea694ef2
CoderCoding00 <107374333+CoderCoding00@users.noreply.github.com>
2023-01-18 21:44:32 -0800

    updated APP.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,67 +1,65 @@
+// Create an Apollo Provider to make every request work with the Apollo Server.
 
-// INSTRUCTIONS FROM README
-// * `App.js`: Create an Apollo Provider to make every request work with the Apollo Server.
-
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // IMPORT APOLLO DEPENDENCIES (ApolloClient, InMemoryCache, ApolloProvider, createHttpLink)
-import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
 // IMPORT THE REACT-ROUTER-DOM DEPENDENCY
-import { setContext } from '@apollo/client/link/context';
-// IMPORTS FROM PAGES AND COMPONENTS WERE ALREADY GIVEN
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
-import Navbar from './components/Navbar';
+import { setContext } from "@apollo/client/link/context";
+// IMPORTS FROM PAGES
+import SearchBooks from "./pages/SearchBooks";
+import SavedBooks from "./pages/SavedBooks";
+import Navbar from "./components/Navbar";
 
-// CREATE A NEW CONST LINK FOR THE createHttpLink
-const link = createHttpLink({
-  uri: '/graphql',
+// CONSTRUCT THE MAIN GRAPHQL API ENDPOINT
+const httpLink = createHttpLink({
+  uri: "/graphql",
 });
 
-// CREATE A NEW CONST AUTH LINK FOR THE setContext
+// CONSTRUCT REQUEST MIDDLEWARE THAT WILL ATTTACH THE JWT TOKEN TO EVERY REQUEST AS AN AUTHORIZATION HEADER
 const authLink = setContext((_, { headers }) => {
-  // GET THE TOKEN 
-  const token = localStorage.getItem('id_token');
-  // RETURN THE HEADERS
+  // GET THE TOKEN FROM LOCAL STORAGE IF IT EXISTS
+  const token = localStorage.getItem("id_token");
+  // RETURN THE HEADERS TO THE CONTEXT SO HTTP LINK CAN READ THEM
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
+
 // CREATE A NEW CONST CLIENT FOR THE ApolloClient
 const client = new ApolloClient({
   // PASS IN THE LINK AND CACHE
-  link: authLink.concat(link),
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
-// REFACTOR THE APP FUNCTION TO WRAP THE ENTIRE APP IN THE ApolloProvider
 function App() {
   return (
-    // WRAP THE ENTIRE APP IN THE ApolloProvider and use "exact" path
+    // WRAP THE ENTIRE APP IN THE ApolloProvider
     <ApolloProvider client={client}>
-    <Router>
-      <>
-        <Navbar />
-        <Routes>
-          <Route 
-            exact path='/' 
-            element={<SearchBooks />} 
-          />
-          <Route 
-            exact path='/saved' 
-            element={<SavedBooks />} 
-          />
-          {/* CHANGE ROUTE RO RENDER INSTEAD???? */}
-          <Route 
-            path='*'
-            element={<h1 className='display-2'>Wrong page!</h1>}
-          />
-        </Routes>
-      </>
-    </Router>
+      <Router>
+        <>
+          <Navbar />
+          <Routes>
+            {/* use "exact" path (OR JUST USE PATH?) */}
+            <Route exact path="/" element={<SearchBooks />} />
+            <Route exact path="/saved" element={<SavedBooks />} />
+            {/* CHANGE ROUTE RO RENDER INSTEAD???? */}
+            <Route
+              path="*"
+              element={<h1 className="display-2">Wrong page!</h1>}
+            />
+          </Routes>
+        </>
+      </Router>
     </ApolloProvider>
   );
 }
